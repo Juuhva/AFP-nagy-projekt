ded7c5cc66da400aa91f44418e7b9c9c2d8d81f0
Hegyközi Petra <petra.hegykozi12345@gmail.com>
2024-11-06 10:58:37 +0100

    Kezdetleges weboldal integrálása a projektbe

---
+++ konyvkucko/resources/views/home/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+@include('home.head')
+</head>
+<body>
+    <header>
+        @include('home.header')
+    </header>
+
+    <section class="main-home">
+        <div class="main-text">
+            <h1>Kiemelt ajánlatunk</h1>
+
+            
+        </div>
+    </section>
+
+    <script src="{{asset('javascript.js')}}"></script>
+</body>
+</html>
