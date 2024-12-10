c247af2607b3189fa1ac6e2f0483b13cc7554655
Hegyközi Petra <petra.hegykozi12345@gmail.com>
2024-12-10 20:46:30 +0100

    Admin felület létrehozása és blade-ekre bontás

---
+++ konyvkucko/resources/views/gyik/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    @include('gyik.head')
+</head>
+<body>
+    <header>
+        @include('gyik.header')
+    </header>
+
+    <section class="main-home">
+        <div class="main-text">
+
+            
+        </div>
+    </section>
+</body>
+</html>
