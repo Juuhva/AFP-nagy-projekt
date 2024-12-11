b5438cfdefbec5fa91d9f839c0a3c4341e2f7f76
CsonkaMihaly <csonkamihaly1999@gmail.com>
2024-12-11 11:45:57 +0100

    Dashboard welcome message kiegészítése

--- konyvkucko/resources/views/dashboard.blade.php
+++ konyvkucko/resources/views/dashboard.blade.php
@@ -3,26 +3,46 @@
 <head>
     @include('home.head')
     <title>@yield('title', 'Custom Dashboard Title')</title> 
+    <style>
+        .main-home {
+            padding: 5% 60% 0% 40%;
+            display:flex;
+        }
+
+        .notification {
+            width: 400px;
+            margin-top: 20px;
+            color: black;
+            font-size: 1.5rem;
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
     <header>
         @include('home.header')
-</header>
+    </header>
 
-
-    <section class="main-home">
+    <section class="main-home" >
         <div class="main-text">
-        @if ($fromLogin)
+            @if ($fromLogin)
+                <div class="notification">
+                @auth
+            <h1>Üdvözlünk, {{ Auth::user()->name }}!</h1>
+        @endauth
+                    <h1>Sikeres bejelentkezés!</h1>
+                </div>
+            @endif
 
-        <div></div>
-    <h1 style="color:black; opacity:1">Sikeres bejelentkezés!</h1>
-@endif
-
-@if ($fromRegister)
-    <h1 style="color:black; opacity:1;" >Sikeres regisztráció!</h1>
-@endif
-    
-    </div>
+            @if ($fromRegister)
+                <div class="notification">
+                @auth
+            <h1>Üdvözlünk, {{ Auth::user()->name }}!</h1>
+        @endauth
+                    <h1>Sikeres regisztráció!</h1>
+                </div>
+            @endif
+        </div>
     </section>
 
     <script src="{{ asset('javascript.js') }}"></script>
